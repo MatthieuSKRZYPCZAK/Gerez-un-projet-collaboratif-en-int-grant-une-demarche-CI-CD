24fed52cafe95f04fe47d4105fff8931c9fd884a
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-07-02 09:32:59 +0200

    Test CI/CD - SOUTENANCE

--- back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
+++ back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
@@ -11,7 +11,7 @@ import com.openclassrooms.bobapp.service.JokeService;
 @RequestMapping("api/joke")
 public class JokeController {
 
-    // Test CI/CD
+    // Test CI/CD soutenance
     private final JokeService jokeService;
 
     JokeController(JokeService jokeService) {
